c4a6ef5a2ddd3baefe8121d4b85ec4a8077929f6
Mateo Olaya Aricapa <mateo.olaya.aricapa@gmail.com>
2024-05-28 14:54:47 -0500

    Estado auth con Redux listo

--- front-end-platform-online/src/router/app-platform.tsx
+++ front-end-platform-online/src/router/app-platform.tsx
@@ -1,12 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRouter } from "./app-router";
+import { Provider } from "react-redux";
+import { store } from "../store/stores";
 
 export const PlatformApp = () => {
 
     return (
-        <BrowserRouter>
-        <AppRouter/>
-        </BrowserRouter>
+        <Provider store={ store }>
+            <BrowserRouter>
+                <AppRouter/>
+            </BrowserRouter>
+        </Provider>
     );
 
 }
--- front-end-platform-online/src/store/stores.ts
+++ front-end-platform-online/src/store/stores.ts
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 
+import { authSlice } from "./slices/auth/auth-slice";
+
 export const store = configureStore({
     reducer: {
-
+        auth: authSlice.reducer,
     },
 });
 
