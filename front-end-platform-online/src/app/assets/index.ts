e0f1e2e56ec441052d99db6284374a08915784b9
Mateo Olaya Aricapa <mateo.olaya.aricapa@gmail.com>
2024-06-03 17:35:32 -0500

    Título, introducción, Field video y Field image agregados

--- front-end-platform-online/src/app/assets/index.ts
+++ front-end-platform-online/src/app/assets/index.ts
@@ -1,9 +1,13 @@
 import iconTime from "./images/tiempo.png";
 import iconSections from "./images/secciones.png";
 import iconClass from "./images/clases.png";
+import iconVideo from "./images/video.png";
+import iconImage from "./images/imagen.png";
 
 export { 
     iconTime, 
     iconSections, 
     iconClass, 
+    iconVideo,
+    iconImage
 };
---
+++ front-end-platform-online/src/app/dashboard/pages/instructor-page/instructor-page.tsx
@@ -0,0 +1,86 @@
+import { Textfield } from '../../../../components/textfield/textfield';
+import './_instructor-page.scss';
+
+import { iconVideo, iconImage } from '../../../assets';
+
+export const InstructorPage = () => {
+
+    return (
+        <div className='instructorPage'>
+
+            { /* Div que contiene todo el contenido HTML */ }
+            <div className='instructorPage__container'>
+                
+                { /* Div que contiene todos los campos para crear un curso */ }
+                <div className='instructorPage__container__createCourse'>
+
+                    { /* Div que contiene el título y introducción del instructor */ }
+                    <div className='instructorPage__container__createCourse__titles'>
+
+                        <h2>Instructor</h2>
+                        <p>
+                            Puedes crear tus propios cursos para publicarlos en la gran comunidad que tiene 
+                            <span> MADEMY</span>. Además, puedes enseñar toda la variedad de temas profesionales.
+                        </p>
+
+                    </div>
+
+                    { /* Div que todos los inputs para crear el curso */ }
+                    <div className='instructorPage__container__createCourse__data'>
+                    
+                        <Textfield 
+                            type='text' 
+                            title='Título' 
+                            placeHolder='Ingresa el título del curso'
+                            className='custom-textfield'
+                        />
+
+                        { /* Div que tiene el fiel y el contenido del video e imagen del curso */ }
+                        <div className='instructorPage__container__createCourse__data__videoImage'>
+
+                            <Textfield 
+                                type='text' 
+                                title='Video introducción' 
+                                placeHolder='Ingresa la URL'
+                                className='custom-textfield'
+                            />
+
+                            <div className='instructorPage__container__createCourse__data__videoImage__content'>
+                                <img src={ iconVideo } alt="" />
+                            </div>
+
+                        </div>
+
+                        { /* Div que tiene el fiel y el contenido del video e imagen del curso */ }
+                        <div className='instructorPage__container__createCourse__data__videoImage'>
+
+                            <Textfield 
+                                type='text' 
+                                title='Imagen de presentación' 
+                                placeHolder='Ingresa la URL'
+                                className='custom-textfield'
+                            />
+
+                            <div className='instructorPage__container__createCourse__data__videoImage__content'>
+                                <img src={ iconImage } alt="" />
+                            </div>
+
+                        </div>
+
+                        
+                    
+                    </div>
+
+                </div>
+
+                { /* Div que la tabla donde se muestra los cursos publicados por el instructor */ }
+                <div className='instructorPage__container__publishCourses'>
+                    hola
+                </div>
+
+            </div>
+
+        </div>
+    );
+
+}
