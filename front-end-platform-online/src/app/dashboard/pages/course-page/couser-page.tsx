2753d70ddb8deaa5d1cac063e223eaad01ae52a5
Mateo Olaya Aricapa <mateo.olaya.aricapa@gmail.com>
2024-06-26 20:55:09 -0500

    Se agregó página que muestra información de un curso

---
+++ front-end-platform-online/src/app/dashboard/pages/course-page/couser-page.tsx
@@ -0,0 +1,13 @@
+import './_course-page.scss';
+
+export const CoursePage = () => {
+
+    return (
+        <div className='coursePage'>
+            Página de curso información
+        </div>
+    );
+
+}
+
+export default CoursePage;
